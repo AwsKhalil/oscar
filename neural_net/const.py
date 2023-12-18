c249990b9213606bd6def4b77dd8c3982d97720c
Aws Khalil <awskh@umich.edu>
2023-12-17 22:09:15 -0500

    Update const.py

--- neural_net/const.py
+++ neural_net/const.py
@@ -4,16 +4,19 @@
 Created on Sat Sep 23 13:23:14 2017
 History:
 11/28/2020: modified for OSCAR 
-
-@author: jaerock
+12/17/2023: New model added by Aws
+            NET_TYPE_STR_THR_BRK
 """
 ###############################################################################
 # constant definition
 
 # network model type
-NET_TYPE_JAEROCK     = 0
-NET_TYPE_JAEROCK_VEL = 2
-NET_TYPE_CE491       = 1
+NET_TYPE_JAEROCK       = 0
+NET_TYPE_CE491         = 1
+NET_TYPE_JAEROCK_VEL   = 2
+NET_TYPE_JAEROCK_SHIFT = 3 
+NET_TYPE_STR_THR_BRK   = 4
+
 NET_TYPE_CONVLSTM    = 10
 
 # file extension
