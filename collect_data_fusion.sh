14fe3554645d0d9489c98d9a2a04a6bd883c7022
Aws Khalil <awskh@umich.edu>
2024-09-12 01:39:41 -0400

    Update collect_data_fusion.sh

--- collect_data_fusion.sh
+++ collect_data_fusion.sh
@@ -3,6 +3,7 @@
 source setup.bash
 
 rosparam set path_to_e2e_data $(pwd)/e2e_fusion_data  # /path/to/data
-rosparam set /use_sim_time true
-#rosrun data_collection data_collection.py $1 /camera/image_raw:=/fusion/front_camera/image_raw
-rosrun data_collection data_collection_sync3.py $1 
+# rosparam set /use_sim_time true
+rosrun data_collection data_collection_delay_mitig.py $1 /camera/image_raw:=/fusion/front_camera/image_raw
+# rosrun data_collection data_collection_sync3.py $1 /camera/image_raw:=/fusion/front_camera/image_raw
+# rosrun data_collection data_collection_nosync_new.py $1 /camera/image_raw:=/fusion/front_camera/image_raw
